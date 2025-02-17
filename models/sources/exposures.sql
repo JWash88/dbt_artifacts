47ddd0e40e4e1a13628fea6fade7f65c4f166b72
Joanna Washington <joanna.washington@deepl.com>
2025-02-17 16:20:46 +0100

    change casting test

--- models/sources/exposures.sql
+++ models/sources/exposures.sql
@@ -7,19 +7,19 @@ with
     )
 
 select
-    cast(null as {{ type_string() }}) as command_invocation_id
-    , cast(null as {{ type_string() }}) as node_id
-    , cast(null as {{ type_timestamp() }}) as run_started_at
-    , cast(null as {{ type_string() }}) as name
-    , cast(null as {{ type_string() }}) as type
-    , cast(null as {{ type_json() }}) as owner
-    , cast(null as {{ type_string() }}) as maturity
-    , cast(null as {{ type_string() }}) as path
-    , cast(null as {{ type_string() }}) as description
-    , cast(null as {{ type_string() }}) as url
-    , cast(null as {{ type_string() }}) as package_name
-    , cast(null as {{ type_array() }}) as depends_on_nodes
-    , cast(null as {{ type_array() }}) as tags
-    , cast(null as {{ type_json() }}) as all_results
+    cast('' as {{ type_string() }}) as command_invocation_id
+    , cast('' as {{ type_string() }}) as node_id
+    , cast('' as {{ type_timestamp() }}) as run_started_at
+    , cast('' as {{ type_string() }}) as name
+    , cast('' as {{ type_string() }}) as type
+    , cast('' as {{ type_json() }}) as owner
+    , cast('' as {{ type_string() }}) as maturity
+    , cast('' as {{ type_string() }}) as path
+    , cast('' as {{ type_string() }}) as description
+    , cast('' as {{ type_string() }}) as url
+    , cast('' as {{ type_string() }}) as package_name
+    , cast('' as {{ type_array() }}) as depends_on_nodes
+    , cast('' as {{ type_array() }}) as tags
+    , cast('' as {{ type_json() }}) as all_results
 from dummy_cte
 where 1 = 0
